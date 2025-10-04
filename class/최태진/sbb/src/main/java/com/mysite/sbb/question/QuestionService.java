d94aa95e4aca94747ed4d769f4b93be89de9431c
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-10-04 23:03:28 +0900

    study: 2장 실습 완료

--- class/최태진/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ class/최태진/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.mysite.sbb.DataNotFoundException;
 
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,13 @@ public class QuestionService {
         else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
