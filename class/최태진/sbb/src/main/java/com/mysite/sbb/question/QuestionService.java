3e9edfbf0333e9e300c94db316dd011a9b06a0e4
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-10-09 14:07:05 +0900

    study: 3장 공부완

--- class/최태진/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ class/최태진/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,9 +1,13 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.user.SiteUser;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import com.mysite.sbb.DataNotFoundException;
 import java.util.ArrayList;
@@ -19,6 +23,25 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
+    private Specification<Question> search(String kw) {
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, SiteUser> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"),
+                        cb.like(q.get("content"), "%" + kw + "%"),
+                        cb.like(u1.get("username"), "%" + kw + "%"),
+                        cb.like(a.get("content"), "%" + kw + "%"),
+                        cb.like(u2.get("username"), "%" + kw + "%"));
+            }
+        };
+    }
+
     public List<Question> getList() {
         return this.questionRepository.findAll();
     }
@@ -33,18 +56,36 @@ public class QuestionService {
         }
     }
 
-    public Page<Question> getList(int page) {
+    public Page<Question> getList(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        Specification<Question> spec = search(kw);
         return this.questionRepository.findAll(pageable);
     }
 
-    public void create(String subject, String content) {
+    public void create(String subject, String content, SiteUser user) {
         Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
+        q.setAuthor(user);
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
+    }
+
+    public void modify(Question question, String subject, String content) {
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+        this.questionRepository.save(question);
+    }
+
+    public void delete(Question question) {
+        this.questionRepository.delete(question);
+    }
+
+    public void vote(Question question, SiteUser siteUser) {
+        question.getVoter().add(siteUser);
+        this.questionRepository.save(question);
     }
 }
--- class/최태진/sbb/src/main/java/com/mysite/sbb/user/UserRole.java
+++ class/최태진/sbb/src/main/java/com/mysite/sbb/user/UserRole.java
@@ -14,4 +14,4 @@ public enum UserRole {
     private String value;
 }
 
-/*파일 생성 groovy로 되어서 문제였음 java로 해야함.*/
+/*파일 생성 groovy로 되어서 문제였음 (패키지 -> 우클릭 enum으로 하니까 왠지 모르겠는데 그루비 파일 만들어짐) java로 해야함.*/
