79c2058678ef427197a77566edf0d0be0a8db7cd
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-10-27 12:15:11 +0900

    feat: Backend Auth 구현 완료

---
+++ server/src/main/java/com/example/server/auth/exception/SupabaseAuthException.java
@@ -0,0 +1,14 @@
+package com.example.server.auth.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public class SupabaseAuthException extends RuntimeException {
+    private final HttpStatusCode statusCode;
+
+    public SupabaseAuthException(String message, HttpStatusCode statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
