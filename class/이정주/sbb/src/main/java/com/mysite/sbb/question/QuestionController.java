adfb6e529637b47db1a56ca811ddc8d242a851f9
leezungzoo <leezungzoo@gmail.com>
2025-11-06 23:18:00 +0900

    [2장종료]

--- class/이정주/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ class/이정주/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,41 +3,47 @@ package com.mysite.sbb.question;
 import java.util.List;
 import java.util.Optional;
 import com.mysite.sbb.DataNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
-        return "question_list";
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "qeustion_form";
     }
 
-    @GetMapping(value = "/question/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionService.getList();
+        return "question_list";
+    }
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable ("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
     }
-
-    public Question getQuestion(Integer id) {
-        Optional<Question> question = this.questionRepository.findById(id);
-        if (question.isPresent()) {
-            return question.get();
-        } else {
-            throw new DataNotFoundException("question not found");
-        }
+    @PostMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
     }
 }
--- class/이정주/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ class/이정주/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,5 +1,10 @@
 package com.mysite.sbb.question;
 
+import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
+
+import java.time.LocalDateTime;
+
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -15,5 +20,22 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
