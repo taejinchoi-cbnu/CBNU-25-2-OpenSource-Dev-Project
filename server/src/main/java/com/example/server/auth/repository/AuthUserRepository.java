c8cba10b32798ccce22e15811cefd3617e5e2721
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-11-06 14:05:28 +0900

    feat(server): 작성자 이름을 UID가 아니라 nickname으로 보여줌

---
+++ server/src/main/java/com/example/server/auth/repository/AuthUserRepository.java
@@ -0,0 +1,13 @@
+package com.example.server.auth.repository;
+
+import com.example.server.auth.entity.AuthUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AuthUserRepository extends JpaRepository<AuthUser, UUID> {
+    Optional<AuthUser> findById(UUID id);
+}
